fc2b90e75f81553710bcdc31478b77cae01d7f4d
An Le <myfriend1226@yahoo.com>
2019-05-27 13:50:21 +1200

    delete unneccesary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import ClothingCard from "./components/ClothingCard";
